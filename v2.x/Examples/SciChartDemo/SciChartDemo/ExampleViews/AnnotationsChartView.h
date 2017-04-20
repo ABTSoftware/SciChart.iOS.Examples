e407775946327b447c7623046d8ccaf5f7dee8fc
andyb1979 <aburnettthompson@gmail.com>
2017-04-20 20:45:36 +0100

    Update AnnotationsChartView.h

--- v2.x/Examples/SciChartDemo/SciChartDemo/ExampleViews/AnnotationsChartView.h
+++ v2.x/Examples/SciChartDemo/SciChartDemo/ExampleViews/AnnotationsChartView.h
@@ -3,7 +3,7 @@
 //  SciChartDemo
 //
 //  Created by Yaroslav Pelyukh on 3/29/16.
-//  Copyright © 2016 SciChart Ltd. All rights reserved.
+//  Copyright © 2016-2017 SciChart Ltd. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>

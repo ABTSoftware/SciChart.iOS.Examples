f83af4cdbffcb6c0f53910960d3f0df448083a9e
yaroslav <yaroslav@scichart.com>
2017-06-08 11:22:45 +0300

    Added example project for third tutorial - adding series

---
+++ v2.x/Tutorials/Xamarin.iOS.C#/Tutorial03_AddingSeries/AddingSeries/AddingSeries/ViewController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using UIKit;
+using SciChart.iOS.Charting;
+
+namespace AddingSeries
+{
+    public partial class ViewController : UIViewController
+    {
+        private SCIChartSurface _surface;
+        private XyDataSeries<Double, Double> _lineDataSeries;
+        private XyDataSeries<Double, Double> _scatterDataSeries;
+
+
+        private SCIFastLineRenderableSeries _lineRenderableSeries;
+        private SCIXyScatterRenderableSeries _scatterRenderableSeries;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            _surface = new SCIChartSurface();
+            _surface.TranslatesAutoresizingMaskIntoConstraints = true;
+            _surface.Frame = this.View.Bounds;
+
+            this.View.AddSubview(_surface);
+
+            _surface.XAxes.Add(new SCINumericAxis());
+            _surface.YAxes.Add(new SCINumericAxis());
+
+            CreateDataSeries();
+            CreateRenderableSeries();
+        }
+
+        void CreateDataSeries()
+        {
+            // Init line data series
+            _lineDataSeries = new XyDataSeries<Double, Double>();
+            for (var i = 0; i < 10; i++)
+            {
+                _lineDataSeries.Append(i, Math.Sin(i));
+            }
+
+            // Init scatter data series
+            _scatterDataSeries = new XyDataSeries<Double, Double>();
+            for (var i = 0; i < 10; i++)
+            {
+                _scatterDataSeries.Append(i, Math.Sin(i));
+            }
+        }
+
+        void CreateRenderableSeries()
+        {
+            _lineRenderableSeries = new SCIFastLineRenderableSeries();
+            _lineRenderableSeries.DataSeries = _lineDataSeries;
+
+
+            _scatterRenderableSeries = new SCIXyScatterRenderableSeries();
+            _scatterRenderableSeries.DataSeries = _scatterDataSeries;
+
+
+            _surface.RenderableSeries.Add(_lineRenderableSeries);
+            _surface.RenderableSeries.Add(_scatterRenderableSeries);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+
+}

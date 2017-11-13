0e2e066c70a6bb5fadfdb76e137a11b4d7a4d608
SciChart DevTeam <devteam@scichart.com>
2017-11-13 05:08:39 -0500

    SciChart.iOS v2.1.0.1827

--- v2.x/Examples/SciChartDemo/SciChartDemo/ExampleViews/PieChartView.m
+++ v2.x/Examples/SciChartDemo/SciChartDemo/ExampleViews/PieChartView.m
@@ -25,8 +25,7 @@
         [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-(0)-[SciChart]-(0)-|" options:0 metrics:0 views:layout]];
         [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(0)-[SciChart]-(0)-|" options:0 metrics:0 views:layout]];
         
-        [self initializeSurfaceRenderableSeries];
-        [self addModifiers];
+        [self initializeSurfaceData];
         
         _pieChartSurface.layoutManager.segmentSpacing = 1;
     }
@@ -34,15 +33,20 @@
     return self;
 }
 
--(void) initializeSurfaceRenderableSeries{
+-(void) initializeSurfaceData{
     SCIPieRenderableSeries * pieChart = [SCIPieRenderableSeries new];
     
-    [pieChart.segments add: [self buildSegmentWithValue:40 Title:@"Green" RadialGradient:[[SCIRadialGradientBrushStyle alloc] initWithColorCodeStart:0xff84BC3D finish:0xff5B8829]]];
-    [pieChart.segments add: [self buildSegmentWithValue:10 Title:@"Red" RadialGradient:[[SCIRadialGradientBrushStyle alloc] initWithColorCodeStart:0xffe04a2f finish:0xffB7161B]]];
-    [pieChart.segments add: [self buildSegmentWithValue:20 Title:@"Blue" RadialGradient:[[SCIRadialGradientBrushStyle alloc] initWithColorCodeStart:0xff4AB6C1 finish:0xff2182AD]]];
-    [pieChart.segments add: [self buildSegmentWithValue:15 Title:@"Yellow" RadialGradient:[[SCIRadialGradientBrushStyle alloc] initWithColorCodeStart:0xffFFFF00 finish:0xfffed325]]];
+    [pieChart.segments add: [self buildSegmentWithValue:40.0 Title:@"Green" RadialGradient:[[SCIRadialGradientBrushStyle alloc] initWithColorCodeStart:0xff84BC3D finish:0xff5B8829]]];
+    [pieChart.segments add: [self buildSegmentWithValue:10.0 Title:@"Red" RadialGradient:[[SCIRadialGradientBrushStyle alloc] initWithColorCodeStart:0xffe04a2f finish:0xffB7161B]]];
+    [pieChart.segments add: [self buildSegmentWithValue:20.0 Title:@"Blue" RadialGradient:[[SCIRadialGradientBrushStyle alloc] initWithColorCodeStart:0xff4AB6C1 finish:0xff2182AD]]];
+    [pieChart.segments add: [self buildSegmentWithValue:15.0 Title:@"Yellow" RadialGradient:[[SCIRadialGradientBrushStyle alloc] initWithColorCodeStart:0xffFFFF00 finish:0xfffed325]]];
     
     [_pieChartSurface.renderableSeries add:pieChart];
+    
+    SCIPieLegendModifier * legendModifier = [[SCIPieLegendModifier alloc] initWithPosition:SCILegendPositionBottom andOrientation:SCIOrientationVertical];
+    legendModifier.pieSeries = pieChart;
+    
+    [_pieChartSurface.chartModifiers add:legendModifier];
 }
 
 -(SCIPieSegment*) buildSegmentWithValue: (double)segmentValue Title:(NSString*)title RadialGradient:(SCIRadialGradientBrushStyle*)gradientBrush{
@@ -53,9 +57,4 @@
     
     return segment;
 }
-
--(void) addModifiers{
-    
-}
-
 @end

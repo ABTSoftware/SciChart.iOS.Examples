422972420cb8b3bf5d51e9538a90f93982363c02
Admin <andrii.polyuk@blackthorn-vision.com>
2017-12-22 16:06:09 +0300

    Sandbox: added obj-c example of custom tick provider

---
+++ v2.x/Sandbox/SciChart_Boilerplate_ObjC_customTicks/SciChart_Boilerplate_ObjC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SciChart_Boilerplate_ObjC
+//
+//  Created by Admin on 02/02/17.
+//  Copyright © 2017 SciChart. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
